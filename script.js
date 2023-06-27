7f6648d23316f6d55a34338066dc3c758ead2e16
Anuj Kumar <Anujk00@hotmail.com>
2023-06-27 15:59:10 -0700

    Added multiple functions to allow the game to run smoothly

--- script.js
+++ script.js
@@ -1,59 +1,112 @@
+// Variables 
 var pOne=0;
 var pTwo=0;
-const playerSelection= "rock";
+var playerSelection;
 var computerSelection;
+var timesPlayed=0;
+var closeBtn= document.getElementById("closeBtn");
+var playAgain= document.getElementById("playAgain");
+var exit= document.getElementById("exit");
+var popup= document.getElementById("popup");
+var playerScore = document.getElementById("playerScore")
+var computerScore= document.getElementById("computerScore");
+var result = document.getElementById("result");
+var resultArr = ["TIE GAME","PAPER BEATS ROCK YOU LOSE","ROCK BEATS SCISSORS YOU WIN",
+"PAPER BEATS ROCK YOU WIN","SCISSORS BEATS PAPER YOU LOSE","SCISSORS BEATS PAPER YOU WIN","ROCK BEATS SCISSORS YOU LOSE"];
+var winnner = document.getElementById("winner");
+
+
+//Function to get random computer choice
 function getComputerChoice(){
-const choice= ["rock", "paper", "scissors"];
-return choice[Math.floor(Math.random()*3)]
+var choice= ["rock", "paper", "scissors","paper", "rock", "scissors"]
+return choice[Math.floor(Math.random()*6)]
+}
+var rock= document.getElementById("rock");
+var paper=document.getElementById("paper");
+var scissors=document.getElementById("scissors");
+
+
+//Event listeners
+rock.addEventListener("click",() =>{
+    playerSelection="rock";
+             game();
+         });
+paper.addEventListener("click",() => {
+    playerSelection="paper";
+             game();
+             
+         });
+ scissors.addEventListener("click",() => {
+    playerSelection="scissors"
+             game();
+             
+         });
+closeBtn.addEventListener("click", () =>{
+            popup.style.display="none";
+        })
+playAgain.addEventListener("click", ()=>{
+    window.location.reload()
+})
+exit.addEventListener("click", () =>{
+    popup.style.display="none";
+})
+
+
+ // Function to end game and display popup asking to play again or exit        
+function game(){
+    playRound(playerSelection,getComputerChoice());
+  if(pOne ==5 || pTwo ==5){
+    if(pOne>pTwo){
+winnner.textContent="Good job my boy"
+    }else{
+winnner.textContent="You a sucka"
+    }
+    popup.style.display="block";
+  }
+
 }
 
+
+// Function for every round played
 function playRound(playerSelection, computerSelection){
-    if(playerSelection=="rock" && computerSelection== "rock"){
-        console.log("TIE GAME");
+    if(pOne <5 && pTwo<5){
+    if(playerSelection==computerSelection){
+        timesPlayed++;
+        result.textContent= resultArr[0];
     }else if(playerSelection=='rock' && computerSelection=="paper" ){
-            console.log("PAPER BEATS ROCK YOU LOSE");
             pTwo++;
+            timesPlayed++;
+            computerScore.textContent="Computer Score : " + pTwo;  
+            result.textContent= "Result: "+resultArr[1];
         }
     else if(playerSelection=='rock' && computerSelection=='scissors'){
-        console.log('ROCK BEATS SCISSORS YOU WIN');
         pOne++;
+        timesPlayed++;
+        playerScore.textContent="Player Score : " + pOne;
+        result.textContent= "Result: "+ resultArr[2];
     }
-
-    if(playerSelection=="paper" && computerSelection== "paper"){
-        console.log("TIE GAME");
-    }else if(playerSelection=='paper' && computerSelection=="rock" ){
-            console.log("PAPER BEATS ROCK YOU WIN");
+    else if(playerSelection=='paper' && computerSelection=="rock" ){
             pOne++;
+            timesPlayed++;
+            playerScore.textContent="Player Score : " + pOne;
+            result.textContent="Result: "+ resultArr[3];
         }
     else if(playerSelection=='paper' && computerSelection=='scissors'){
-        console.log('SCISSORS BEATS PAPER YOU LOSE');
         pTwo++;
-    }
-    if(playerSelection=="scissors" && computerSelection== "scissors"){
-        console.log("TIE GAME");
+        timesPlayed++;
+        computerScore.textContent="Computer Score : " + pTwo;
+        result.textContent="Result: "+ resultArr[4];
     }else if(playerSelection=='scissors' && computerSelection=="paper" ){
-            console.log("SCISSORS BEATS PAPER YOU WIN");
             pOne++;
+            timesPlayed++;
+            playerScore.textContent="Player Score : " + pOne;
+            result.textContent="Result: "+ resultArr[5];
         }
     else if(playerSelection=='scissors' && computerSelection=='rock'){
-        console.log('ROCK BEATS SCISSORS YOU LOSE');
         pTwo++;
+        timesPlayed++;
+        computerScore.textContent="Computer Score : " + pTwo;
+        result.textContent="Result: "+ resultArr[6];
     }
-    }
-
-    function playGame(){
-        for(var i =0; i<5; i++){
-            console.log('Game '+i);
-            computerSelection=getComputerChoice();
-             console.log( "Player choice = " + playerSelection);
-     console.log('Computer choice = ' + computerSelection);
-            playRound(playerSelection,computerSelection);
-        }
-        if(pOne>pTwo){
-            console.log("You Win");
-        }else if(pOne>pTwo){
-            console.log("You Lose");
-        }
-    }
-    console.log(playGame());
-
+}
+}
